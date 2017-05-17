aa13c5c014456183dc7a8295ecf323585947f7b6
Colin Daniels <colin.r.daniels@gmail.com>
2017-05-17 10:54:30 -0400

    Fixed read/write errors in poscar files due to vec3_t refactor.

--- src/common/io/file_types/poscar.cpp
+++ src/common/io/file_types/poscar.cpp
@@ -112,7 +112,7 @@ bool io::read_poscar(std::string filename, structure_t &output)
         {
             auto positions = io::split_line(line);
 
-            vec3_t pos;
+            vec3_t pos = {};
             for (int i = 0; i < 3; ++i)
             {
                 double val = stod(positions[i]);
@@ -189,7 +189,7 @@ bool io::write_poscar(std::string filename, const structure_t &input)
     fbc::invert_3x3(input.lattice, inv_lattice);
     outfile << "Direct\n";
 
-    for (std::size_t i = 0; i < positions.size(); i += 3)
+    for (std::size_t i = 0; i < positions.size(); ++i)
     {
         for (int j = 0; j < 3; ++j)
         {
